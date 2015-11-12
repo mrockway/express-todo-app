299b978b73a24abc53bb45eca62250396f6609e8
mrockway <mrockway@gmail.com>
2015-11-12 00:24:11 -0800

    get and post working

--- public/scripts.js
+++ public/scripts.js
@@ -2,27 +2,32 @@ console.log("10-4 little buddy");
 
 //set up variable for data
 // handlebars template and source
+
 var source = $('#task-template').html();
 var template = Handlebars.compile(source);
-var todoResults = [];
 
-//rerender page on C, U, D
-function render() {
+var allTasks = [];
+
+var render = function() {
 	$('.todoList').empty();
-	var todoHtml = template({data : data});
+	var todoHtml = template({data : allTasks});
 	$('.todoList').append(todoHtml);
-}
+};
 
 $.get('http://localhost:3000/api/todos/', function(data) {
 	todoResults = template({data : data});
+	allTasks = data;
 	$('.todoList').append(todoResults);
+	return todoResults;
 });
 
 $('.todoForm').on('submit',function(event) {
 	event.preventDefault();
 	var newTask = $(this).serialize();
+
 	$.post('http://localhost:3000/api/todos/',newTask, function(data) {
-		todoResults.push(newTask);
+		console.log(data);
+		allTasks.push(data);
 		render();
 	});
 	 
